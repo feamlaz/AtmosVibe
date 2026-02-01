9f9de87b1d9a6fa94a927736e1d47ef4e8200463
feamlaz <120783371+feamlaz@users.noreply.github.com>
2026-02-01 18:48:26 +0300

    Work1

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,14 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/AtmosVibe/',
+  base: './',
   plugins: [react()],
   build: {
     outDir: 'dist',
     rollupOptions: {
+      input: './src/main.jsx',
       output: {
-        format: 'umd',
+        format: 'iife',
         entryFileNames: 'assets/[name].js',
         chunkFileNames: 'assets/[name].js',
         assetFileNames: 'assets/[name].[ext]'
