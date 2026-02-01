e7a7613132c4ed4aaac2f85807a9cf15079328fe
feamlaz <120783371+feamlaz@users.noreply.github.com>
2026-02-01 18:22:20 +0300

    Off manifect 1

--- vite.config.js
+++ vite.config.js
@@ -1,45 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import svgr from 'vite-plugin-svgr';
-import { resolve } from 'path';
 
 export default defineConfig({
   base: '/AtmosVibe/',
-  plugins: [
-    react({
-      babel: {
-        plugins: []
-      }
-    }),
-    svgr()
-  ],
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src')
-    }
-  },
-  server: {
-    port: 3000,
-    host: 'localhost',
-    open: true,
-    hmr: false,
-    cors: true
-  },
+  plugins: [react()],
   build: {
     outDir: 'dist',
-    sourcemap: false,
-    minify: 'esbuild',
-    target: 'es2015',
     rollupOptions: {
       output: {
-        format: 'iife',
-        manualChunks: undefined,
         entryFileNames: 'assets/[name].js',
         chunkFileNames: 'assets/[name].js',
         assetFileNames: 'assets/[name].[ext]'
       }
-    },
-    assetsInlineLimit: 0,
-    cssCodeSplit: false
+    }
   }
 });
